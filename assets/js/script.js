2254081e1c472dcb4fdb98b3ed3384cf164b59fb
Moses <PRiMA@Mosess-MacBook-Pro.local>
2023-03-01 17:52:42 +0000

    js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,160 +1,108 @@
-// To implement
-    // Display current day at top of page when user opens planner
-
-
-// Initialise javascript variables
-var currentDay = moment().format("dddd D MMMM");
-var timeblockContainer = $(".container");
-
-
-var setDate = function () {
-    // Set text of current day to moment value
-    $("#currentDay").text(currentDay);      
-}
-setDate();
-    // Present individual timeblocks for 9/5 
-
-var createTimeblocks = function () { 
-
-    // Create array to hold hours data 
-    var timeOfDay = [9,10,11,12,13,14,15,16,17];
-
-    // Create table to hold all the rows of timeblocks
-    var newTable = $("<table>");
-    // Add styling to make table 100% width
-    newTable.attr("style","width:100%");
-
-    // Append table element to time block container
-    timeblockContainer.append(newTable);
-    timeblockContainer.addClass("time-block");
-
-    // For loop to run create row for each hour 
-    for (var i = 0; i < timeOfDay.length; i++){
-
-        // TABLE ROW
-
-
-        // Create table row
-        var timeblockRow = $("<tr>");
-        timeblockRow.attr("class","row");
-        // Append to table element
-        newTable.append(timeblockRow);
-
-
-
-        // TIME FRAME
-
-
-        // Create table description for time frame
-        var tableTime = $("<td>");
-        // Add styling to make table 15% width
-        tableTime.attr("style","width:15%");
-        tableTime.addClass("hour");
-        // Set current time variable to hold hour format of number in timeOfDay array
-        var currentTime = moment(timeOfDay[i],"Ha").format("Ha");
-        // Set text to current time
-        tableTime.text(currentTime);
-        // Append table row to description block
-        timeblockRow.append(tableTime);
-
-
-        // USER INPUT
-
-
-        // Add rows into table
-        var tableInput = $("<td>");
-        tableInput.attr("style","width:70%");
-
-        // Add colour pallettes to past/present
-            // Use stringify to convert moment and time into comparable values
-
-        // Initialise comparable values
-      
-        var todayDate = moment([timeOfDay[i]], 'HH');
-        var pastDate = moment();
-    
-
-        var userInput = $("<textarea>");
-        userInput.attr("style","width:100%");
-        userInput.attr("id", i);
-
-
-
-
-
-        tableInput.append(userInput);
-        timeblockRow.append(tableInput);
-
-
-        
-        if (todayDate.isBefore(pastDate)) {
-            tableInput.addClass("past");
-        } else if(todayDate.isAfter(pastDate)){
-            tableInput.addClass("future");
-        } else{
-            tableInput.addClass("present");
-        }
-
-        
-        
-
-        // SAVE BUTTON
-
-
-        //  Add rows into table
-        var tableButton = $("<td>");
-        tableButton.attr("style","width:15%");
-
-        // Create button to save 
-        var saveButton = $("<input>");
-        saveButton.attr("class","saveBtn");
-        saveButton.attr("type","submit");
-        saveButton.attr("value","Submit");
-        saveButton.text("Save event");
-        saveButton.attr("id",i);
-        saveButton.addClass("btn-lg");
-
-        // Append button and desc to timeblock 
-        tableButton.append(saveButton);
-        timeblockRow.append(tableButton);
-
-
-        saveButton.on("click",function(event){ 
-
-            event.stopPropagation();
-            event.preventDefault();
-            // Initialise variables to hold value of button clicked
-            var element = event.target;
-            
-            var userInput = localStorage.getItem("userInput");
-            console.log(userInput);
-            
-            
-        
-        })
-      
-
-
-    }
-
-}
-
-createTimeblocks();
-
+$(document).ready(function () {
+    // * Display the current day at the top of the calender when a user opens the planner.
+    let currentDate = $("#currentDay");
+    var today = moment();
+    currentDate.text(today.format("dddd, MMMM D YYYY"));
   
-
-
-    // Allow user to enter event when click on timeblock
-     
-    // Save event in local storage when save button clicked on individual timeblock
-
-    // Persist events between refreshes of a page
-
-
-
-
-    // Change class on timeblock based on if its before/at/after current time
-        // How?
-            // Get current time in numeric format
-
-            // Get chosen time in numeric format; 
+    // Save the events and the time corresponding to that event as an object.
+  
+    let events = {};
+    let hourRendered = moment();
+  
+    loadPlanner();
+    startPlanner();
+  
+    // Render and display the daily planner
+    // The rows of the planner must start with 9 and end with 5.
+    // Initialise the starting hour
+    function renderPlanner(today, events) {
+      let hourRow = moment(today).hour(9); // This will start the rows from 9
+      let planner = $(".container");
+      planner.empty();
+  
+      // Increment the number of rows by 1 and display it on the page
+      // Increment until 'X' number of rows is displayed
+  
+      // * Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+  
+      for (i = 0; i < 9; i++) {
+        let row = $("<div>").addClass("row");
+  
+        let time = "";
+        if (today.isBefore(hourRow, "hour")) {
+          time = "future";
+        } else if (today.isAfter(hourRow, "hour")) {
+          time = "past";
+        } else {
+          time = "present";
+        }
+  
+        // Append each row to the planner with classes from bootstrap to style the row
+  
+        planner.append(row);
+        row.append($("<div>").addClass("col-2 hour").text(hourRow.format("h A")));
+        let timeBlock = hourRow.format("hA");
+  
+        // * Allow a user to enter an event when they click a timeblock
+        row.append(
+          $("<textarea>").addClass(`col-8 ${time}`).text(events[timeBlock])
+        );
+        row.append(
+          $("<button>")
+            .addClass("col-2 saveBtn")
+            .html("<i class='fas fa-save'></i>")
+            .attr("aria-label", "Save")
+            .attr("id", hourRow.format("hA"))
+        );
+  
+        // * Present timeblocks for standard business hours when the user scrolls down.
+        // Increment the timings between each row by 1.
+  
+        hourRow.add(1, "hour");
+  
+        hourRendered = moment();
+      }
+    }
+  
+    // Load and Render the planner
+    function startPlanner() {
+      renderPlanner(today, events);
+    }
+  
+    // Load and render the events that was stored in localStorage; display the item stored in localStorage
+    function loadPlanner() {
+      let storedEvents = JSON.parse(localStorage.getItem("events"));
+      if (storedEvents) {
+        events = storedEvents;
+      }
+    }
+  
+    
+    hourTracker();
+  
+    // * Persist events between refreshes of a page
+  
+    function hourTracker() {
+      setInterval(function () {
+        if (moment().isAfter(hourRendered, "minute")) {
+          startPlanner();
+        }
+      }, 600000);
+    }
+  
+    // Store events with local Storage
+  
+    function savePlanner() {
+      localStorage.setItem("events", JSON.stringify(events));
+    }
+  
+    // Save the event using the save button
+  
+    // * Save the event in local storage when the save button is clicked in that timeblock.
+  
+    $(document).on("click", ".saveBtn", function (e) {
+      let plannerEvent = e.currentTarget.parentElement.children[1].value;
+      events[e.currentTarget.id] = plannerEvent;
+      savePlanner();
+    });
+  });
